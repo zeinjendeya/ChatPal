c90d1afec758897966a3b7b981a5c4dc87a8d85d
Moheb Makramallah <mmakramallah@alriyadh.gov.sa>
2026-07-09 16:29:26 +0300

    deployment edits

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,15 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 import { authRoutes, messagesRoutes } from "./routes/index.js";
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
+
 const port = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messagesRoutes);
+
+//deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
