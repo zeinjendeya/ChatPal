11ec69984b57972f1ed198762c76e3fbe6786fe0
zeinjendeya <z.jendeya@gmail.com>
2026-07-10 14:03:17 +0300

    type checks

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,7 +1,11 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
-  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+  const { JWT_SECRET } = process.env;
+  if (!JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined in environment variables");
+  }
+  const token = jwt.sign({ userId }, JWT_SECRET, {
     expiresIn: "7d",
   });
 
