6c49f84e20f3db42fd7d463f5254ecddf1a763ab
zeinjendeya <z.jendeya@gmail.com>
2026-07-12 17:40:27 +0300

    changes: 1- update profile route 2- create protection middleware

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,15 +1,27 @@
 import express from "express";
-import { signup, login, logout } from "../controllers/auth.controller.js";
 import {
-	signupValidation,
-	loginValidation,
-	validate,
+  signup,
+  login,
+  logout,
+  updateProfile,
+} from "../controllers/auth.controller.js";
+import {
+  signupValidation,
+  loginValidation,
+  validate,
 } from "../validators/auth.validator.js";
+import { protectRoute } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signupValidation, validate, signup);
 router.post("/login", loginValidation, validate, login);
 router.post("/logout", logout);
+
+router.put("/update-profile", protectRoute, updateProfile);
+
+router.get("/check", protectRoute, (req, res) => {
+  res.status(200).json(req.user);
+});
 
 export default router;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
+import cookieParser from "cookie-parser";
 
 import { authRoutes, messagesRoutes } from "./routes/index.js";
 import { connectDB } from "./lib/db.js";
@@ -15,6 +16,7 @@ const __dirname = path.resolve();
 const port = ENV.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messagesRoutes);
